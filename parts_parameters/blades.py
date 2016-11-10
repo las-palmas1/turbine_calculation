11b3349332735cd284fe6254c67ebc5910df8f7a
las-palmas1 <las-palmas1@mail.ru>
2016-11-10 20:23:05 +0300

    Add files via upload

--- parts_parameters/blades.py
+++ parts_parameters/blades.py
@@ -23,4 +23,19 @@ for n1, i1 in enumerate(stages):
             rk_blades[n1][k_point_name] = NXExpression('Point', k_point_name, [i2.x_k[n3], i2.y_k[n3], i2.r], mm_unit)
 
 
+sa_blades = []
 
+for n1, i1 in enumerate(stages):
+    sa_blades.append(dict())
+    sa_blades[n1]['D1_in'] = NXExpression(number_type, 'D1_in', i1['sa']['D0_in'], mm_unit)
+    sa_blades[n1]['D2_in'] = NXExpression(number_type, 'D2_in', i1['sa']['D05_in'], mm_unit)
+    sa_blades[n1]['D1_out'] = NXExpression(number_type, 'D1_out', i1['sa']['D0_out'], mm_unit)
+    sa_blades[n1]['D2_out'] = NXExpression(number_type, 'D2_out', i1['sa']['D05_out'], mm_unit)
+    sa_blades[n1]['z'] = NXExpression(integer_type, 'z', i1['rk']['z'], nd_unit)
+    sa_blades[n1]['b_a'] = NXExpression(number_type, 'b_a', i1['rk']['b_a'], mm_unit)
+    for n2, i2 in enumerate(i1['sa']['sections']):
+        for n3, i3 in enumerate(i2.x_s):
+            s_point_name = 's%s_%s' % (n3, n2)
+            k_point_name = 'k%s_%s' % (n3, n2)
+            sa_blades[n1][s_point_name] = NXExpression('Point', s_point_name, [i2.x_s[n3], i2.y_s[n3], i2.r], mm_unit)
+            sa_blades[n1][k_point_name] = NXExpression('Point', k_point_name, [i2.x_k[n3], i2.y_k[n3], i2.r], mm_unit)
--- parts_parameters/main.py
+++ parts_parameters/main.py
@@ -1,4 +1,4 @@
-from parts_parameters.rk_blades import rk_blades
+from parts_parameters.blades import rk_blades, sa_blades
 import parts_parameters.func as func
 import os
 
@@ -6,3 +6,6 @@ models_path = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(__fil
 
 for n, i in enumerate(rk_blades):
     func.create_expressions_file('st%s_rk_blade' % (n + 1), os.path.join(models_path, 'st%s_rk_blade.prt' % (n + 1)), i)
+
+for n, i in enumerate(sa_blades):
+    func.create_expressions_file('st%s_sa_blade' % (n + 1), os.path.join(models_path, 'st%s_sa_blade.prt' % (n + 1)), i)
